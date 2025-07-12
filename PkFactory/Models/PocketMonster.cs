36cf98dd1b3736570f32f7a67d8ef7e2ec5aba4e
William Jackson <will-jj@users.noreply.github.com>
2025-07-12 11:05:32 +0100

    Refactory, adding of saves

--- PkFactory/Models/PocketMonster.cs
+++ PkFactory/Models/PocketMonster.cs
@@ -1,38 +1,11 @@
 using System.Collections.ObjectModel;
-using System.Dynamic;
 using System.Linq;
 using CommunityToolkit.Mvvm.ComponentModel;
 using PkFactory.ViewModels;
 
 namespace PkFactory.Models;
 
-public partial class Set : ViewModelBase
-{
-    [ObservableProperty]
-    string _showdownText = string.Empty;
-
-    [ObservableProperty]
-    private bool _isNotValid;
-    
-    [ObservableProperty]
-    string _errors = string.Empty;
-    
-    public uint? PID { get; set; }
-
-    public Set()
-    {
-    }
-
-    public Set(string showdownText, uint? pID = null)
-    {
-        ShowdownText = showdownText;
-        IsNotValid = false;
-        PID = pID;
-    }
-}
-
-
-public partial class Poketmonster : ViewModelBase
+public partial class PocketMonster : ViewModelBase
 {
     [ObservableProperty]
     string _frontierMon = string.Empty;
@@ -49,6 +22,10 @@ public partial class Poketmonster : ViewModelBase
     [ObservableProperty]
     private int? _ivs;
     
+    
+    [ObservableProperty]
+    private string _ivStr;
+    
     public ObservableCollection<string> Mons { get; set; }
 
     public ObservableCollection<int> IvRs { get; set; } = new(Enumerable.Range(0, 32));
@@ -56,10 +33,11 @@ public partial class Poketmonster : ViewModelBase
     public ObservableCollection<int> Abilities { get; set; } = new(Enumerable.Range(0, 2));
 
 
-    public Poketmonster(string[] poketmonsters, string placeHolder)
+    public PocketMonster(string[] pocketMonsters, string placeHolder, string ivStr)
     {
-        Mons  = new(poketmonsters);
+        Mons  = new(pocketMonsters);
         PlaceHolder =  placeHolder;
+        IvStr = ivStr;
     }
     
 }
--- PkFactory/ViewModels/MainViewModel.cs
+++ PkFactory/ViewModels/MainViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Linq;
 using static System.Buffers.Binary.BinaryPrimitives;
@@ -27,58 +26,33 @@ public partial class MainViewModel : ViewModelBase
     private string? _filename = "emerald-factory.sav";
     
 
-    private string _name = string.Empty;
-
     private SaveFile? _saveFile;
-
-
-
-    [ObservableProperty]
-    private string? _selectedGame;
-
-
-    [Required]
-    [MinLength(1)]
-    [MaxLength(7)]
-    public string Name
-    {
-        get => _name;
-        set => SetProperty(ref _name, value, true);
-    }
-
-
-
-    [ObservableProperty]
-    private int _selectedAbility;
     
+    private enum Levels
+    {
+        Singles50,
+        Singles100,
+        Doubles50,
+        Doubles100,
+        OwnSave
+    }
+    
+    public ObservableCollection<PocketMonster> Poketmonsters { get; set; } = new()
+    {
+        new PocketMonster(Constants.FrontierMons.FrontierMonNames, "Mon 1", "IVs [31]"),
+        new PocketMonster(Constants.FrontierMons.FrontierMonNames, "Mon 2", "IVs [31]"),
+        new PocketMonster(Constants.FrontierMons.FrontierMonNames, "Mon 3", "IVs [31]"),
+        new PocketMonster(Constants.FrontierMons.FrontierMonNames, "Opp 1", "IVs [3]"),
+        new PocketMonster(Constants.FrontierMons.FrontierMonNames, "Opp 2", "IVs [3]"),
+        new PocketMonster(Constants.FrontierMons.FrontierMonNames, "Opp 3", "IVs [3]"),
+    };
+    
+
     [ObservableProperty]
     private int _selectedLevel;
-    
-    [ObservableProperty]
-    private int _selectedIv;
-    
-    [ObservableProperty]
-    private string _selectedFrontierMon;
-
-
-    public ObservableCollection<Poketmonster> Poketmonsters { get; set; } = new()
-    {
-        new Poketmonster(Constants.FrontierMons.FrontierMonNames, "Mon 1"),
-        new Poketmonster(Constants.FrontierMons.FrontierMonNames, "Mon 2"),
-        new Poketmonster(Constants.FrontierMons.FrontierMonNames, "Mon 3"),
-        new Poketmonster(Constants.FrontierMons.FrontierMonNames, "Opp 1"),
-        new Poketmonster(Constants.FrontierMons.FrontierMonNames, "Opp 2"),
-        new Poketmonster(Constants.FrontierMons.FrontierMonNames, "Opp 3"),
-    };
 
     [ObservableProperty]
-    private string _selectedSet;
-
-    [ObservableProperty]
-    private int _selectedSetIndex;
-
-    [ObservableProperty]
-    private bool _includeTeam;
+    private bool _useOwnSave;
 
     [RelayCommand]
     public async Task GetPreppedFile(string asset)
@@ -90,17 +64,7 @@ public partial class MainViewModel : ViewModelBase
         _saveFile = SaveUtil.GetVariantSAV(byteArray);
         if (_saveFile != null) CanSaveFile = true;
     }
-
-    public async Task LoadFileFromDisk(string path)
-    {
-        Stream datain = File.OpenRead(path);
-        using MemoryStream memoryStream = new();
-        await datain.CopyToAsync(memoryStream);
-        byte[] byteArray = memoryStream.ToArray();
-        _saveFile = SaveUtil.GetVariantSAV(byteArray);
-        if (_saveFile != null) CanSaveFile = true;
-    }
-
+    
     [RelayCommand]
     public async Task GetFile()
     {
@@ -117,26 +81,12 @@ public partial class MainViewModel : ViewModelBase
             using MemoryStream memoryStream = new();
             await datain.CopyToAsync(memoryStream);
             byte[] byteArray = memoryStream.ToArray();
-            int real = 131072;
-            var data = byteArray[..real].ToArray();
-            var hmm = data[0xE70];
-            var hmm2 = data[0xE71];
             _saveFile = SaveUtil.GetVariantSAV(byteArray);
-            if (_saveFile != null)
+            if (_saveFile is SAV3E)
             {
                 CanSaveFile = true;
-                Name = _saveFile.OT;
             }
         }
-
-
-        if (_saveFile is not SAV3E save) return;
-        int record = 0xE70;
-
-
-        const int flag = 0xEFA;
-        save.Small[flag] = 1;
-        
     }
 
     private static void WriteMonToSave(SAV3E save, ref int record, ushort frontierIdx, uint naturality, byte ivs, byte ability)
@@ -158,29 +108,34 @@ public partial class MainViewModel : ViewModelBase
         save.Small[record] = ability;
         record++;
         record += 2;
-        
-        
-        const int flag = 0xEFA;
-        save.Small[flag] = 1;
     }
     
     [RelayCommand]
     public async Task DoIt()
     {
-        if (SelectedLevel == 0)
+        switch (SelectedLevel)
         {
-            await GetPreppedFile("avares://PkFactory/Assets/lv50.sav");
-        }
-        else
-        {
-            await GetPreppedFile("avares://PkFactory/Assets/ol.sav");
-
+            case (int)Levels.Singles50:
+                await GetPreppedFile("avares://PkFactory/Assets/pokeemerald-50.sav");
+                break;
+            case (int)Levels.Singles100:
+                await GetPreppedFile("avares://PkFactory/Assets/pokeemerald-ol.sav");
+                break;
+            case (int)Levels.Doubles50:
+                await GetPreppedFile("avares://PkFactory/Assets/pokeemerald-d50.sav");
+                break;
+            case (int)Levels.Doubles100:
+                await GetPreppedFile("avares://PkFactory/Assets/pokeemerald-d100.sav");
+                break;
+            case (int)Levels.OwnSave:
+                if(!CanSaveFile) return;
+                break;
         }
 
         if (_saveFile is not SAV3E save) return;
         int record = 0xE70;
         int ii = 0;
-        foreach (Poketmonster monster in Poketmonsters)
+        foreach (PocketMonster monster in Poketmonsters)
         {
             // find the string because binding is silly
             int index = Array.IndexOf(Constants.FrontierMons.FrontierMonNames, monster.FrontierMon);
@@ -192,13 +147,15 @@ public partial class MainViewModel : ViewModelBase
             WriteMonToSave(save, ref record, (ushort)index, (uint)nature, (byte)monster.Ivs, (byte)monster.Ability);
         }
         
+        const int flag = 0xEFA;
+        save.Small[flag] = 1;
+        
         await SaveFile();
     }
 
     [RelayCommand(CanExecute = nameof(CanSaveFile))]
     public async Task SaveFile()
     {
-
         TopLevel? topLevel = DialogManager.GetTopLevelForContext(this);
         if (topLevel == null) return;
         FilePickerSaveOptions options = new()
@@ -206,10 +163,15 @@ public partial class MainViewModel : ViewModelBase
             SuggestedFileName = _filename
         };
         IStorageFile? fileOut = await topLevel.StorageProvider.SaveFilePickerAsync(options);
-        if (fileOut != null)
+        if (fileOut != null && _saveFile != null)
         {
             await using Stream stream = await fileOut.OpenWriteAsync();
             await stream.WriteAsync(_saveFile.Write());
         }
+    }
+
+    partial void OnSelectedLevelChanged(int value)
+    {
+        UseOwnSave = value == (int)Levels.OwnSave;
     }
 }
